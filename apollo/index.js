fd913b5b8912c4ccc6bb51286c3052ba5cedd3ba
Simei Alves <simeiparreiras@gmail.com>
2023-06-23 16:49:40 -0300

    feat: adicionando GraphQL

--- apollo/index.js
+++ apollo/index.js
@@ -2,76 +2,109 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import axios from "axios";
 
-const books = [
-  {
-    id: "a",
-    title: "Harry Potter and the Chamber of Secrets",
-    author: "J.K Rowling",
-  },
-  {
-    id: "b",
-    title: "Jurassic Park",
-    author: "Michael Crichton",
-  },
-];
+async function getAllSurvey() {
+  return await axios
+    .get("https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/survey")
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log("Erro ao buscar as enquetes:", error);
+      return [];
+    });
+}
 
-const survey = await axios
-  .get("https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/survey")
-  .then((response) => response.data)
-  .catch((error) => {
-    console.log("Erro ao buscar as enquetes:", error);
-    return [];
-  });
+async function getAllSurveyItems() {
+  return await axios
+    .get("https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/survey-items")
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log("Erro ao buscar as enquetes:", error);
+      return [];
+    });
+}
 
-const survey_items = await axios
-  .get("https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/survey-items")
-  .then((response) => response.data)
-  .catch((error) => {
-    console.log("Erro ao buscar as enquetes:", error);
-    return [];
-  });
-
-const poll_items = await axios
-  .get("https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/poll-items")
-  .then((response) => response.data)
-  .catch((error) => {
-    console.log("Erro ao buscar as enquetes:", error);
-    return [];
-  });
-
-console.log(survey, survey_items, poll_items);
+async function getAllPollItems() {
+  return await axios
+    .get("https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/poll-items")
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log("Erro ao buscar as enquetes:", error);
+      return [];
+    });
+}
 
 const typeDefs = `#graphql
-    type Book {
-        id: String
-        title: String
-        author: String
+   
+    type Survey {
+      surveyId: String
+      name: String
+      status: String
     }
     
+    type SurveyItems {
+      surveyItemId: String
+      description: String
+      surveyId: String
+      vote: String
+    }
+
+    type PollItems{
+      pollItemId: String
+      surveyItemId: String
+      description: String
+      surveyId: String
+    }
+
     type Query{
-        books: [Book]
+      survey: [Survey]
+      survey_items: [SurveyItems]
+      poll_items: [PollItems]
     }
 
     type Mutation {
-        addBook(id: String, title: String, author: String): Book
-        deleteBook(id: String): Book
+        addSurvey(surveyId: String, name: String, status: String): Survey
+        deleteSurvey(surveyId: String): Survey
+
+        addSurveyItems(surveyItemId: String, description: String, surveyId: String, vote: String): SurveyItems
+        deleteSurveyItems(surveyItemId: String): SurveyItems
+
+        addPollItems(pollItemId: String, surveyItemId: String, description: String, surveyId: String): PollItems
+        deletePollItems(pollItemId: String): PollItems                
     }
     `;
 
 const resolvers = {
   Query: {
-    books: () => books,
+    survey: () => getAllSurvey(),
+    survey_items: () => getAllSurveyItems(),
+    poll_items: () => getAllPollItems(),
   },
   Mutation: {
-    addBook: (_, { id, title, author }) => {
-      const book = { id, title, author };
-      books.push(book);
-      return book;
+    addSurvey: async (_, { name, status }) => {
+      try {
+        const response = await axios.post(
+          `https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/survey`,
+          {
+            name: name,
+            status: status,
+          }
+        );
+
+        return response.data;
+      } catch (error) {
+        throw new Error("Failed to add survey");
+      }
     },
-    deleteBook: (_, { id }) => {
-      const book = books.find((book) => book.id === id);
-      books.splice(books.indexOf(book), 1);
-      return book;
+
+    deleteSurvey: async (_, { surveyId }) => {
+      try {
+        const response = await axios.delete(
+          `https://kofgvsu30l.execute-api.us-east-1.amazonaws.com/survey/${surveyId}`
+        );
+
+        return response.data;
+      } catch (error) {
+        throw new Error("Falha ao excluir a enquete");
+      }
     },
   },
 };
